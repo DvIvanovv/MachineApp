ac458447b488cdcaac9833b3100b74b343db0ce1
DvIvanovv <d_v_ivanov@abv.bg>
2018-07-26 20:57:17 +0300

    Empti spring project

---
+++ MachineApp/src/main/java/machine/MachineAppApplication.java
@@ -0,0 +1,12 @@
+package machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineAppApplication.class, args);
+	}
+}
---
+++ MachineApp/src/test/java/machine/MachineAppApplicationTests.java
@@ -0,0 +1,16 @@
+package machine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MachineAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
